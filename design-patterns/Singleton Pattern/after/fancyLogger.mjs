4935c310b3e4057ade1d811fc6f24e10b1fd3f4a
Bart Kooijman <lpkooijman@gmail.com>
2022-08-10 16:28:28 +0200

    Update fancyLogger.mjs

--- design-patterns/Singleton Pattern/after/fancyLogger.mjs
+++ design-patterns/Singleton Pattern/after/fancyLogger.mjs
@@ -19,5 +19,13 @@ class FancyLogger {
 
 const logger = new FancyLogger()
 Object.freeze(logger)
+/**
+ * The Object.freeze() method freezes an object. Freezing an object prevents 
+ * extensions and makes existing properties non-writable and non-configurable.
+ * A frozen object can no longer be changed: new properties cannot be added, 
+ * existing properties cannot be removed, their enumerability, configurability, 
+ * writability, or value cannot be changed, and the object's prototype 
+ * cannot be re-assigned. freeze() returns the same object that was passed in.
+ **/ 
 
 export default logger
