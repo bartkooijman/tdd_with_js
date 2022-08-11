12fd4fe6e14e396d29bdf19de6103df21ff51e98
Bart Kooijman <lpkooijman@gmail.com>
2022-08-11 11:22:57 +0200

    massive change :-)

--- excercise8/NewsAgency.mjs
+++
@@ -1,4 +0,0 @@
-export default class NewsAgency {
-  receiveNewsItem(newsItem) {
-  }
-}
--- maintainable-code-excercises/excercise8-solution/index.mjs
+++ maintainable-code-excercises/excercise8-solution/index.mjs
@@ -1,9 +1,14 @@
-import NewsAgency from "./NewsAgency.mjs";
+import newsAgency from "./NewsAgency.mjs";
 import NewsChannel from "./NewsChannel.mjs";
 
 const infoQ = new NewsChannel("InfoQ");
 const dZone = new NewsChannel("DZone");
 
-const newsAgency = new NewsAgency();
+//No longer needed when it becomes singleton
+//const newsAgency = new NewsAgency();
+
+//solution
+newsAgency.subscribe(infoQ)
+newsAgency.subscribe(dZone)
 
 newsAgency.receiveNewsItem("New version of JavaScript released ES3000");
