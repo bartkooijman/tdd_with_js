248521c11a83eb4413c14ad457692d724c0ff482
Bart Kooijman <lpkooijman@gmail.com>
2023-11-06 16:49:08 +0100

    minor fixes

--- training-excercises/excercise3-solution/src/vloercalculator.js
+++ training-excercises/excercise3-solution/src/vloercalculator.js
@@ -2,8 +2,6 @@ const { getOppervlaktePerPak } = require ("./oppervlaktePerPak")
 
 async function aantalPakketten(lengte, breedte, productNummer, oppervlaktePerPak = getOppervlaktePerPak()) {
   const oppervlakteUitPak = await oppervlaktePerPak(productNummer)
-  console.log(oppervlakteUitPak)
-  console.log(lengte * breedte / oppervlakteUitPak)
   return Math.ceil(lengte * breedte / oppervlakteUitPak)
 }
 
--- training-excercises/excercise3/src/vloercalculator.js
+++ training-excercises/excercise3/src/vloercalculator.js
@@ -1,8 +1,8 @@
 const { getOppervlaktePerPak } = require ("./oppervlaktePerPak")
 
 async function aantalPakketten(lengte, breedte, productNummer) {
-  const oppervlaktePerPak = await getOppervlaktePerPak(productNummer)
-  return Math.ceil(lengte * breedte / oppervlaktePerPak)
+  const oppervlakteUitPak = await getOppervlaktePerPak(productNummer)
+  return Math.ceil(lengte * breedte / oppervlakteUitPak)
 }
 
 module.exports = { aantalPakketten }
