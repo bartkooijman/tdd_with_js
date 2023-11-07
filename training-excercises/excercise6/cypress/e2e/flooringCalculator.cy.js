22f43a77551b2ce10db1706e9d43ea5ff4813718
Bart Kooijman <lpkooijman@gmail.com>
2023-11-07 17:40:56 +0100

    new cypress version

--- training-excercises/excercise6/cypress/e2e/flooringCalculator.cy.js
+++ training-excercises/excercise6/cypress/e2e/flooringCalculator.cy.js
@@ -1,4 +1,4 @@
-describe('flooring calculator on GNL spec', () => {
+describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.gamma.nl/assortiment/gamma-signature-xtra-breed-laminaat-wit-gerookt-eiken-2v-groef-8-mm-2-69-m2/p/B582169')
     cy.get(".js-calculate-field").clear()
@@ -7,5 +7,15 @@ describe('flooring calculator on GNL spec', () => {
     cy.get(".js-calculate-button").click()
     cy.wait(2000);
     cy.get(".js-calculator-result-amount-number").should("have.text", "3")
+  })
+
+  it('fails', () => {
+    cy.visit('https://www.gamma.nl/')
+    //Queries: https://docs.cypress.io/api/commands/as 
+    //Actions: https://docs.cypress.io/api/commands/type
+    //Assertions: https://docs.cypress.io/api/commands/should
+    cy.get("h2").should("have.text", "Waar ben je naar op zoek?")
+    cy.get("#reactheader form")
+    cy.get("p:first")
   })
 })
